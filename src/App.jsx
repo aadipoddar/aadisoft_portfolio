2b644cebb2ba096d64aa381a159e9d0334aff2d0
Aadi Poddar <aadipoddarmail@gmail.com>
2025-05-30 16:26:06 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () =>{
+    return (
+        <main>
+            <div>
+                <h1 className="text-3xl underline">Welcome to my 3D portfolio</h1>
+            </div>
+        </main>
+    )
+}
+
+export default App
